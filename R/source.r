04cdfe5da83c12e8dff1695d1e151878b283a47b
Sooyong Lee <sooyongl09@utexas.edu>
2021-03-10 21:48:03 -0600

    test functions
    기본적인 것들만 만들어 봄

---
+++ R/source.r
@@ -0,0 +1,77 @@
+#' @import lavaan
+#' @import tidyverse
+#' @importFrom MASS mvrnorm
+#' @importFrom glue glue
+NULL
+
+suppressPackageStartupMessages(library(lavaan))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(MASS))
+suppressPackageStartupMessages(library(glue))
+
+#' @makeCondition
+#
+makeCondition <- function() {
+
+
+
+}
+
+
+#' @genData
+#'
+genData <- function() {
+
+
+}
+
+#' @dataCut
+#'
+dataCut <- function() {
+
+
+}
+
+#' @analysis
+#'
+analysis <- function() {
+
+
+}
+
+
+#' @getPower
+#'
+getPower <- function() {
+
+
+}
+
+#' @getBias
+#'
+getBias <- function() {
+
+
+}
+
+
+#' @runSimulation
+#'
+runSimulation <- function() {
+
+}
+
+
+#' @makePlot
+#'
+makePlot <- function() {
+
+
+}
+
+
+#' @makeTable
+#'
+
+
+
