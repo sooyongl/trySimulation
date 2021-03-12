a64b91a286357a8cdb102f3a558c6203f3aba42c
Sooyong Lee <sooyongl09@utexas.edu>
2021-03-12 12:28:57 -0600

    new presentation

---
+++ R/trySimulation.r
@@ -0,0 +1,10 @@
+#' trySimulation: A package for practicing a simulation study with R.
+#'
+#' The trySimulation pacakge provides something.
+#'
+#' @section the functions:
+#' the functions something something
+#'
+#' @docType package
+#' @name trySimulation
+NULL
